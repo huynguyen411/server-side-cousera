2b695e04fc2b641854ffe377752ce6677266dd77
Huy Nguyen <huyscopio007@gmail.com>
2021-01-31 09:31:37 +0700

    facebook token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '482751065983711',
+    clientSecret: '8497a5acd4bc2431b7363119b18aad0a'
+  }
 }
